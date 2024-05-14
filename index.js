fbb8c8db72abf0a74fd5a1645f2e56e1d0b4e384
redrumkumg <n.ronnapat@github.com>
2024-05-14 14:08:08 +0700

    apply

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+const a = 10;
+const b = 5;
+
+console.log(add(a, b));
+console.log(subtract(a, b));
+console.log(multiply(a, b));
+console.log(divide(a, b));
+
+Inform = (firstName, location, hobby) => {
+  console.log(
+    "Hi, my name is " +
+      firstName +
+      " I live in " +
+      location +
+      " and enjoy " +
+      hobby
+  );
+};
+
+let firstName = "Ronnapat";
+let location = "Bangkok";
+let hobby = "Watching movie";
+
+Inform(firstName, location, hobby);
